0ed8d2c2f10890185cd63b4a0a5e29a04ac0bf5a
Roma1504 <romachkagir@yandex.ru>
2025-02-27 18:22:15 +0300

    page new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import './page.css';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+
+// Определяем тип для TelegramWebApp (замените 'any' на более конкретный тип, если возможно)
+type TelegramWebAppType = any; // Или null
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <h1>Лодддддд</h1>
-    </div>
-  );
+    const [userId, setUserId] = useState(0);
+    const [TelegramWebApp, setTelegramWebApp] = useState<TelegramWebAppType | null>(null); // Состояние для TelegramWebApp
+
+    useEffect(() => {
+        // Динамический импорт библиотеки
+        import('@twa-dev/sdk')
+            .then((module) => {
+                const TWA = module.default;
+                setTelegramWebApp(TWA); // Сохраняем TWA в состоянии
+                TWA.ready();
+                TWA.expand();
+            })
+            .catch((error) => {
+                console.error('Failed to load TelegramWebApp:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (TelegramWebApp) {
+            const user = TelegramWebApp.initDataUnsafe?.user;
+
+            console.log(user);
+            if (user) {
+                setUserId(user.id);
+            }
+        }
+    }, [TelegramWebApp]); // Зависимость от TelegramWebApp
+
+    return (
+        <div className="backround">
+            <h1>Jsdkdkalk</h1>
+        </div>
+    );
 }
